c8b73a3b74b497275209bf3bd7eade3650a046db
MatanAvitan-98 <matan3751@gmail.com>
2022-09-25 02:59:09 +0300

    add web fotter

--- src/components/webFooter.js
+++ src/components/webFooter.js
@@ -6,7 +6,10 @@ export default function WebFooter() {
     return (
         <Card className="web-footer">
             <Card.Title dir="rtl" className='hebrew'>
-מוקדש לעילוי נשמת שלום אביטן ז"ל נבל"ע כ"ד בשבט תשע"ח ת.נ.צ.ב.ה
+                מוקדש לעילוי נשמת שלום אביטן ז"ל נבל"ע כ"ד בשבט תשע"ח ת.נ.צ.ב.ה
+            </Card.Title>
+            <Card.Title  dir="rtl" className='hebrew'>
+                מוקדש לעילוי נשמת אברהם גפני בן יוסף וחממה ז"ל נבל"ע כ' בסיון תשע"ז ת.נ.צ.ב.ה
             </Card.Title>
 
         </Card>
