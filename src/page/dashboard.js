26e594e8699bbd5c8b2bd67d692ce62a87ba45a3
MatanAvitan-98 <matan3751@gmail.com>
2022-09-25 03:42:18 +0300

    change to time5

--- src/page/dashboard.js
+++ src/page/dashboard.js
@@ -149,7 +149,7 @@ function Dashboard() {
                 <Carousel  controls={false} indicators={false}>
                     <Carousel.Item interval={6000}>
                         <img  className='carousel-size'
-                            src={require('./time4.png')}
+                            src={require('./time5.png')}
                         />
                     </Carousel.Item>
 
